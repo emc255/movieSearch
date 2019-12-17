e69c4ad2a1ad032b6b4132ce6d7d8f0a0dbb08df
emc <micha3l_255@yahoo.com>
2019-12-16 18:26:15 -0600

    add movie poster and add more info about the movie

--- apps.js
+++ apps.js
@@ -2,11 +2,11 @@
 let datas = [];
 
 document.querySelector(".submit-btn").addEventListener("click", startSearch);
+document.querySelector(".box").addEventListener("click", moreInfo);
 
 function startSearch() {
   const search = document.querySelector(".search").value;
   getData(search);
-  getPoster(search);
 }
 
 function getData(search) {
@@ -22,7 +22,8 @@ function getData(search) {
          <div class="container_title">
          <p>Title: ${data.Title}</p>
          <p>Type: ${data.Type}</p>
-         <img class="image" src="" alt="">
+         <img class="image" src=${data.Poster} alt="">
+         <button class="more-info-btn" data-id=${data.imdbID}>more info</button>
          </div>
         `
       );
@@ -33,15 +34,42 @@ function getData(search) {
   xhttp.send();
 }
 
-function getPoster(search) {
-  const xhttp = new XMLHttpRequest();
-  const image = document.querySelectorAll(".image");
+function moreInfo(e) {
+  datas.Search.forEach(data => {
+    if (e.target.dataset.id === data.imdbID) {
+      getInfo(e.target.dataset.id);
+    }
+  });
+}
 
+function getInfo(search) {
+  const xhttp = new XMLHttpRequest();
+  console.log(search);
   xhttp.onload = function() {
-    const poster = this.response;
-    console.log(poster);
+    const infos = JSON.parse(this.response);
+
+    document.querySelector("search").insertAdjacentHTML(
+      "beforeend",
+      `  
+        <p>Actors: ${infos.Actors}</p>
+        <p>Type: ${infos.Plot}</p>
+        <p>Metascore: ${infos.Metascore}</p>
+        <p class="ry">Release Year: ${infos.Year}</p>
+        `
+    );
+
+    for (const [key, value] of Object.entries(infos.Ratings[0])) {
+      document.querySelector(".ry").insertAdjacentHTML(
+        "beforeend",
+        `
+        <p>Rating: ${key}:${value}</p>
+        `
+      );
+    }
+
+    console.log(infos);
   };
 
-  xhttp.open("GET", `http://img.omdbapi.com/?s=${search}&apikey=8cb964c5`, true);
+  xhttp.open("GET", `http://www.omdbapi.com/?i=${search}&apikey=8cb964c5`, true);
   xhttp.send();
 }
